6b23885f5ccf02303f2f3fdcf5555b7fd1ca6389
Victor <vcrads2014@gmail.com>
2018-07-09 06:32:59 +0000

    first controllers and modules

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'pages/show'
+
+  get 'home/show'
+  
+  resources :pages, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ db/migrate/20180709014704_create_pages.rb
@@ -0,0 +1,14 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.string :title
+      t.integer :section_id
+      t.text :body
+      t.integer :order
+      t.boolean :is_published
+      t.boolean :menu_display
+
+      t.timestamps null: false
+    end
+  end
+end

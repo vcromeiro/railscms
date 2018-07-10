44f2487c05bfd7d7d709fca36e7221bcfbc55b73
Victor <vcrads2014@gmail.com>
2018-07-10 06:35:18 +0000

    Front-end retrieving data from db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :getPageNav
+  before_filter :getPageNav, :getJumbotron, :getBlocks
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,5 +7,13 @@ class ApplicationController < ActionController::Base
   
   def getPageNav
     @pageNav = Page.where("menu_display = true && is_published = true").order(order: :asc)
+  end
+  
+  def getJumbotron
+    @jumbotron = Block.where("position = 'jumbotron'")
+  end
+  
+  def getBlocks
+    @blocks = Block.where("position = 'block'").order(order: :asc)
   end
 end
